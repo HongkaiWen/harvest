6c8acbf4debf029403157c7c1e45d0b3fc3204b5
hongkai <446410466@qq.com>
2017-08-28 12:19:10 +0800

    init

---
+++ src/main/java/com/github/hongkaiwen/harvest/BootApplication.java
@@ -0,0 +1,14 @@
+package com.github.hongkaiwen.harvest;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by hongkai on 2017/8/28.
+ */
+public class BootApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
