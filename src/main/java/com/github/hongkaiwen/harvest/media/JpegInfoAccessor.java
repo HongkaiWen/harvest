fb8f3419bed04f10413c1605b03891d92cd0a9d2
hongkai <446410466@qq.com>
2017-09-27 11:26:57 +0800

    代码架构搭建

--- src/main/java/com/github/hongkaiwen/harvest/media/JpegInfoAccessor.java
+++ src/main/java/com/github/hongkaiwen/harvest/media/JpegInfoAccessor.java
@@ -3,6 +3,7 @@ package com.github.hongkaiwen.harvest.media;
 import com.drew.imaging.jpeg.JpegMetadataReader;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.file.FileMetadataDirectory;
 
 import java.io.File;
 import java.util.Date;
@@ -22,14 +23,18 @@ public class JpegInfoAccessor implements MediaInfoAccessor {
     public Date createTime() {
         try{
             Metadata metadata = JpegMetadataReader.readMetadata(this.mediaFile);
-            return metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class).getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+            Date photoTime = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class).getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+            if(photoTime == null){
+                photoTime = metadata.getFirstDirectoryOfType(FileMetadataDirectory.class).getDate(FileMetadataDirectory.TAG_FILE_MODIFIED_DATE);
+            }
+            return photoTime;
         }catch(Exception e){
             return null;
         }
     }
 
     public static void main(String args[]){
-        Date time = new JpegInfoAccessor(new File("E:/私人文件夹/照片170114/IMG_20160721_204518.jpg")).createTime();
+        Date time = new JpegInfoAccessor(new File("E:/私人文件夹/照片170114/IMG_20161112_130719.jpg")).createTime();
         System.out.println(time);
     }
 }
